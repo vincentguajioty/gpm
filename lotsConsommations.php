810af535387fae126118510e2a2397c520393e73
Vincent Guajioty <contact@guajioty.fr>
2021-10-10 16:13:57 +0200

    Dates sur les rapports de consommation

--- lotsConsommations.php
+++ lotsConsommations.php
@@ -72,7 +72,7 @@ if ($_SESSION['consommationLots_lecture']==0)
                         <div class="box box-info collapsed-box">
                             <div class="box-header with-border">
                                 <h3 class="box-title">
-                                    <i class="fa fa-heartbeat"></i> <?= $data['evenementConsommation'] ?>
+                                    <i class="fa fa-heartbeat"></i> <?= $data['dateConsommation'] ?> <?= $data['evenementConsommation'] ?>
                                 </h3>
                                 <div class="box-tools pull-right">
                                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
