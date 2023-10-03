3ed400f12c1b7186ca387864dffbcc7872001b45
Vincent Guajioty <contact@guajioty.fr>
2023-10-03 09:06:01 +0200

    Icone et nom d'app

--- frontend/src/components/common/Logo.js
+++ frontend/src/components/common/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-import logo from 'assets/img/illustrations/falcon.png';
+import logo from 'assets/img/logo.png';
 
 const Logo = ({ at, width, className, textClass, ...rest }) => {
   return (
@@ -27,7 +27,7 @@ const Logo = ({ at, width, className, textClass, ...rest }) => {
         )}
       >
         <img className="me-2" src={logo} alt="Logo" width={width} />
-        <span className={classNames('font-sans-serif', textClass)}>falcon</span>
+        <span className={classNames('font-sans-serif', textClass)}>{process.env.REACT_APP_NAME}</span>
       </div>
     </Link>
   );
