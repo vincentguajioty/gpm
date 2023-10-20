916221e4c3f3c77905588090eed21ac2ca87dffb
Vincent Guajioty <contact@guajioty.fr>
2023-10-20 12:40:48 +0200

    Docker build

--- buildAllDocker.ps1
+++ buildAllDocker.ps1
@@ -8,16 +8,12 @@ Invoke-Expression $command
 cd ..
 cd frontend
 
-$command = "docker image build . -f DockerFile-UAT -t gpm-fo-uat:"+$version
-Invoke-Expression $command
-$command = "docker image build . -f DockerFile-PROD -t gpm-fo-prod:"+$version
+$command = "docker image build . -f Dockerfile -t gpm-fo:"+$version
 Invoke-Expression $command
 
 cd ..
 
 $command = "docker save gpm-bo:"+$version+" -o gpm-bo-"+$version+".tar"
 Invoke-Expression $command
-$command = "docker save gpm-fo-uat:"+$version+" -o gpm-fo-uat-"+$version+".tar"
-Invoke-Expression $command
-$command = "docker save gpm-fo-prod:"+$version+" -o gpm-fo-prod-"+$version+".tar"
+$command = "docker save gpm-fo:"+$version+" -o gpm-fo-"+$version+".tar"
 Invoke-Expression $command
