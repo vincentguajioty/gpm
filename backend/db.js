defaf2c185ab3c7772fce1aa942f9cbe095dd1b5
Vincent Guajioty <contact@guajioty.fr>
2023-10-20 12:40:36 +0200

    Spec port DB BO

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,7 @@ const logger = require('./winstonLogger');
 
 const db = mariadb.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
