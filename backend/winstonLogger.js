e6fc38ede765d257bcab1fd04b1ce012fd8fe38c
Vincent Guajioty <contact@guajioty.fr>
2023-12-22 10:20:49 +0100

    Remove logs collors

--- backend/winstonLogger.js
+++ backend/winstonLogger.js
@@ -1,34 +1,12 @@
-// const { format, createLogger, transports } = require('winston');
-// const { timestamp, combine, errors, json } = format;
-
-// const logger = createLogger({
-//     level: process.env.LOG_LEVEL,
-//     format: combine(
-//         timestamp(),
-//         errors({stack:true}),
-//         json(),
-//     ),
-//     transports: [
-//         new transports.Console(),
-//         new transports.File({
-//             filename: 'logs/logs.log',
-//             maxsize: 5242880,
-//             maxFiles: 5,
-//         }),
-//     ],
-// });
-
-// module.exports = logger;
-
 const { format, createLogger, transports } = require('winston');
-const { combine, colorize, timestamp, align, printf, json, errors } = format;
+const { combine, timestamp, align, printf, json, errors } = format;
 
 const DailyRotateFile = require("winston-daily-rotate-file");
 
 const logFormat = combine(
-    colorize(),
     timestamp(),
     json(),
+    align(),
     errors({stack:true}),
     printf(
         info => `[${info.timestamp}][${info.level}][${info.idPersonne > 0 ? 'idPersonne: '+info.idPersonne : 'SYSTEM'}][${info.identifiant ? 'identifiant: '+info.identifiant : 'SYSTEM'}][delegation: ${info.tokenDelegationActive && info.tokenDelegationActive == true ? true : false}][userInitial: ${info.tokenDelegationActive && info.tokenDelegationActive == true ? info.tokenDelegationInitialIdPersonne+' '+info.tokenDelegationInitialIdentifiant : 'N/A'}] ${info.message}${info.backupBeforeDrop ? ' '+JSON.stringify(info.backupBeforeDrop) : ''}`,
