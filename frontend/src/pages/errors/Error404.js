fb4fc9e81977dcf9a1fa703fa000dac11e8be4d4
Vincent Guajioty <contact@guajioty.fr>
2023-12-28 15:45:48 +0100

    Lottie sur pages d'erreur

--- frontend/src/pages/errors/Error404.js
+++ frontend/src/pages/errors/Error404.js
@@ -3,12 +3,18 @@ import { Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import Lottie from 'lottie-react';
+import lottie404 from 'components/widgets/lottie-error400';
 
 const Error404 = () => {
   return (
     <Card className="text-center">
       <Card.Body className="p-5">
-        <div className="display-1 text-300 fs-error">404</div>
+        <center>
+          <div className="display-1 text-300 fs-error w-75">
+            <Lottie animationData={lottie404} loop={true} />
+          </div>
+        </center>
         <hr />
         <p className="lead mt-4 text-800 font-sans-serif fw-semi-bold">
           La page demandée n'existe pas
--- frontend/src/pages/errors/Error500.js
+++ frontend/src/pages/errors/Error500.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import Lottie from 'lottie-react';
+import lottie500 from 'components/widgets/lottie-error500';
 
 const Error500 = () => (
-  <Card className="text-center h-100">
+  <Card className="text-center">
     <Card.Body className="p-5">
-      <div className="display-1 text-300 fs-error">500</div>
+      <center>
+        <div className="display-1 text-300 fs-error w-75">
+          <Lottie animationData={lottie500} loop={true} />
+        </div>
+      </center>
       <hr />
       <p className="lead mt-4 text-800 font-sans-serif fw-semi-bold">
         Oups, erreur technique !
       </p>
+      <Link className="btn btn-primary btn-sm mt-3" to="/">
+        <FontAwesomeIcon icon={faHome} className="me-2" />
+        Retour à l'accueil
+      </Link>
     </Card.Body>
   </Card>
 );
