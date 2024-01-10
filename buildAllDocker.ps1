9fe29ab08fba99744d2f769a0fcdba30b7e9f76a
Vincent Guajioty <contact@guajioty.fr>
2024-01-10 08:28:31 +0100

    Tag Latests sur les livraisons docker

--- buildAllDocker.ps1
+++ buildAllDocker.ps1
@@ -2,13 +2,13 @@ param([string]$version)
 
 cd backend
 
-$command = "docker image build . -t gpm-bo:"+$version
+$command = "docker image build . -t gpm-bo:"+$version+" -t gpm-bo:latest"
 Invoke-Expression $command
 
 cd ..
 cd frontend
 
-$command = "docker image build . -f Dockerfile -t gpm-fo:"+$version
+$command = "docker image build . -f Dockerfile -t gpm-fo:"+$version+" -t gpm-fo:latest"
 Invoke-Expression $command
 
 cd ..
@@ -18,7 +18,16 @@ Invoke-Expression $command
 $command = "docker save gpm-fo:"+$version+" -o gpm-fo-"+$version+".tar"
 Invoke-Expression $command
 
+$command = "docker save gpm-bo:latest -o gpm-bo-latest.tar"
+Invoke-Expression $command
+$command = "docker save gpm-fo:latest -o gpm-fo-latest.tar"
+Invoke-Expression $command
+
 $command = "docker rmi gpm-bo:"+$version
 Invoke-Expression $command
 $command = "docker rmi gpm-fo:"+$version
+Invoke-Expression $command
+$command = "docker rmi gpm-bo:latest"
+Invoke-Expression $command
+$command = "docker rmi gpm-fo:latest"
 Invoke-Expression $command
